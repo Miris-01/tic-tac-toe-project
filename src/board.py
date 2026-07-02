0f9ea38b80ae0a63506ecd15449ebea8a97aa885
miriam <miriamga1994@gmail.com>
2026-07-02 11:35:16 +0200

    worked on check_win and update_board function

--- src/board.py
+++ src/board.py
@@ -9,11 +9,28 @@ def create_board():
 
 
 def display_board(board):
+    #si es solamente para imprimir, podemos usar row
+    for row in board:
+        print(row)
 
-    for i in range():
-        print(board)
+def update_board(coord: tuple[int, int], simbol, board):
+    #separamos los valores de las coordenadas en filas y columnas
+    fila, columna = coord
 
-#def update_board():
+    board[fila][columna] = simbol
+    display_board(board)
+
+    return board
 
 
-#def check_win():
+def check_win(board, simbol):
+
+    diag_win_1 = ((board.diagonal()) == simbol).all( )
+    diag_win_2 = ((np.fliplr(board).diagonal()) == simbol).all()
+    row_win = (board == simbol).all(axis=1).any()
+    column_win = (board == simbol).all(axis=0).any()
+
+    if diag_win_1 or diag_win_2 or row_win or column_win: 
+        print(f'El jugador {simbol} gana la partida!!!')
+        return True
+    return False
